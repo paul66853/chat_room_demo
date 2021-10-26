2c79378c0cb190fd0149fde05550d5fc1c592c8d
paul66853 <paul66853@gmail.com>
2021-10-26 11:33:36 +0800

    DEMO_01完成

--- app/src/main/java/com/chat_room_demo/ChatActivity.kt
+++ app/src/main/java/com/chat_room_demo/ChatActivity.kt
@@ -1,11 +1,92 @@
 package com.chat_room_demo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
 
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var chatRecyclerView: RecyclerView
+    private lateinit var messageBox: EditText
+    private lateinit var sendButton: ImageView
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var messageList: ArrayList<Message>
+    private lateinit var mDbRef: DatabaseReference
+
+    var receiverRoom: String ?= null
+    var senderRoom: String ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+
+        val name = intent.getStringExtra("name")
+        val receverUid = intent.getStringExtra("uid")
+
+        val senderUid = FirebaseAuth.getInstance().currentUser?.uid
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+
+        senderRoom = receverUid + senderUid
+        receiverRoom = senderUid + receverUid
+
+        supportActionBar?.title = name
+
+        chatRecyclerView = findViewById(R.id.chatRecyclerView)
+        messageBox = findViewById(R.id.messageBox)
+        sendButton = findViewById(R.id.sentButton)
+        messageList = ArrayList()
+        messageAdapter = MessageAdapter(this,messageList)
+
+        chatRecyclerView.layoutManager = LinearLayoutManager(this)
+        chatRecyclerView.adapter = messageAdapter
+
+        //新增畫面上的訊息到recyclerView(原本只有傳到資料庫，還未顯示)
+        mDbRef.child("chats").child(senderRoom!!).child("message")
+            .addValueEventListener(object :ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    messageList.clear()
+
+                    for(postSnapshot in snapshot.children){
+
+                        val message = postSnapshot.getValue(Message::class.java)
+                        messageList.add(message!!)
+
+                    }
+                    messageAdapter.notifyDataSetChanged()
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+
+
+        //新增訊息到資料庫
+        sendButton.setOnClickListener{
+
+            val message = messageBox.text.toString()
+            //message檔案格式
+            val messageObject = Message(message,senderUid)
+            //資料庫新增節點
+            mDbRef.child("chats").child(senderRoom!!).child("message").push()
+                .setValue(messageObject).addOnSuccessListener {
+                    mDbRef.child("chats").child(receiverRoom!!).child("message").push()
+                        .setValue(messageObject)
+                }
+            messageBox.setText("")
+        }
+
     }
 }
